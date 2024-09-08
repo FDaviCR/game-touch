d9b76d933ff9da272b7d6ab806a12598f61615b1
Davi <davicamiloribeiro@gmail.com>
2024-09-08 16:54:04 -0300

    Apresentar tempo de jogo na tela

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ tela = pygame.display.set_mode((largura, altura))
 points = 0
 
 COR_FUNDO = (30, 30, 30)
-WHITE = (255, 255, 255)
+COR_FONTE = (255, 255, 255)
 player = Player()
 
 bots = []
@@ -27,10 +27,18 @@ clock = pygame.time.Clock()
 
 font = pygame.font.SysFont("Comic Sans", 25)
 score = 0
+start_time = time.time()
 
 def showScore():
-    scoreText = font.render("Pontos: " + str(score), True, WHITE)
+    scoreText = font.render("Pontos: " + str(score), True, COR_FONTE)
     tela.blit(scoreText, (150, 0))
+    
+def showTimer():
+    elapsedTime = time.time() - start_time
+    hours, remainder = divmod(elapsedTime, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    scoreText = font.render(str(f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"), True, COR_FONTE)
+    tela.blit(scoreText, (700, 0))
 
 while executando:
     clock.tick(60)
@@ -58,6 +66,7 @@ while executando:
     todos_sprites.draw(tela)
 
     showScore()
+    showTimer()
     pygame.display.flip()
 
 pygame.quit()
